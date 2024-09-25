793ca77264fb7377cebf471dceea31c13fa331e6
Blitzkriegtech <aljunrendon@yahoo.com>
2024-09-25 14:01:40 +0800

    Add declaration of variables

---
+++ main.js
@@ -0,0 +1,6 @@
+
+let weapon = prompt("Please enter your weapon of choice");
+const playerDisplay = document.getElementById("playerDisplay");
+const computerDisplay = document.getElementById("computerDisplay");
+const resultDisplay = document.getElementById("resultDisplay");
+
